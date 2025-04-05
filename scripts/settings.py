28c20d1716153929d48787c33c22a8ed2d538e76
Nemesix493 <verquerre.kevin@gmail.com>
2025-04-05 21:31:44 +0200

    feat: add script settings file

---
+++ scripts/settings.py
@@ -0,0 +1,12 @@
+
+"""
+This module define the globals settings for the scripts
+"""
+
+from pathlib import Path
+
+# project directory path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# cad files directory path
+CAD_FILES_DIR = BASE_DIR / "cad"
