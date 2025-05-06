4a0e84aa1032c8a47dd4f355544eb2fb7c776e3e
Nemesix493 <verquerre.kevin@gmail.com>
2025-05-06 21:00:53 +0200

    test: add tests for bom generation

--- scripts/doc_generation/tests/__init__.py
+++ scripts/doc_generation/tests/__init__.py
@@ -6,8 +6,11 @@ Exposes test case classes for import and discovery by test runners.
 
 from .document import DocumentTests
 from .project_info import ProjectInfoTests
+from .bom_generation import BOMDocumentTest, RunBOMGenerationTest
 
 __all__ = [
     'DocumentTests',
-    'ProjectInfoTests'
+    'ProjectInfoTests',
+    'BOMDocumentTest',
+    'RunBOMGenerationTest'
 ]
